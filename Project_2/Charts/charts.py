b8f0f0c6552216a57f73a8d60e299d7e6252aabc
Cranhat <cyprian.burdzy@gmail.com>
2025-05-05 13:15:20 +0200

    added automatically saving charts to pdf

--- Project_2/Charts/charts.py
+++ Project_2/Charts/charts.py
@@ -5,9 +5,11 @@ from pathlib import Path
 import numpy as np
 
 plt.rcParams.update({'font.size': 18})
-# path = "../Results/"
 
-path = "C:/Users/Cyprian/Projects/Data-structures-academic-project/Project_2/Results/"
+
+# path = "C:/Users/Cyprian/Projects/Data-structures-academic-project/Project_2/Results/"
+path = "C:/Users/cypri/Projects/Data-structures-academic-project/Project_2/Results/"
+
 
 files = ["ar changeKey.csv", "ar extractMax.csv", "ar getSize.csv", "ar insert.csv", "ar peek.csv", "hp changeKey.csv", "hp extractMax.csv", "hp getSize.csv", "hp insert.csv", "hp peek.csv"]
 data = {}
@@ -21,14 +23,16 @@ fig, ax = plt.subplots(figsize=(20, 10))
 for i, element in enumerate(data):
     if "ar" in element:
         ax.plot(data[element]["count"], data[element]["time"], 'o-', label = element)
-
+title = "Wszystkie operacje dla tablicy dynamicznej"
 ax.grid()
 ax.set(xticks = data["ar changeKey"]["count"])
-ax.set_title("Wszystkie operacje dla tablicy dynamicznej")
+ax.set_title(title)
 plt.legend(fontsize = "x-large")
 plt.xlabel("count")
 plt.ylabel("time [ns]")
+plt.savefig(title + ".pdf", format="pdf") 
 plt.show()
+
 
 
 fig, ax = plt.subplots(figsize=(20, 10))
@@ -37,12 +41,14 @@ for i, element in enumerate(data):
     if "hp" in element:
         ax.plot(data[element]["count"], data[element]["time"], 'o-', label = element)
 
+title = "Wszystkie operacje dla kopca"
 ax.grid()
 ax.set(xticks = data["ar changeKey"]["count"])
-ax.set_title("Wszystkie operacje dla kopca")
+ax.set_title(title)
 plt.legend(fontsize = "x-large")
 plt.xlabel("count")
 plt.ylabel("time [ns]")
+plt.savefig(title + ".pdf", format="pdf") 
 plt.show()
 
 fig, ax = plt.subplots(figsize=(20, 10))
@@ -50,12 +56,14 @@ for i, element in enumerate(data):
     if "changeKey" in element:
         ax.plot(data[element]["count"], data[element]["time"], 'o-', label = element)
 
+title = "Operacja changeKey dla wszystkich struktur"
 ax.grid()
 ax.set(xticks = data["ar changeKey"]["count"])
-ax.set_title("Operacja changeKey dla wszystkich struktur")
+ax.set_title(title)
 plt.legend(fontsize = "x-large")
 plt.xlabel("count")
 plt.ylabel("time [ns]")
+plt.savefig(title + ".pdf", format="pdf") 
 plt.show()
 
 fig, ax = plt.subplots(figsize=(20, 10))
@@ -63,12 +71,14 @@ for i, element in enumerate(data):
     if "extractMax" in element:
         ax.plot(data[element]["count"], data[element]["time"], 'o-', label = element)
 
+title = "Operacja extractMax dla wszystkich struktur"
 ax.grid()
 ax.set(xticks = data["ar changeKey"]["count"])
-ax.set_title("Operacja extractMax dla wszystkich struktur")
+ax.set_title(title)
 plt.legend(fontsize = "x-large")
 plt.xlabel("count")
 plt.ylabel("time [ns]")
+plt.savefig(title + ".pdf", format="pdf") 
 plt.show()
 
 fig, ax = plt.subplots(figsize=(20, 10))
@@ -76,12 +86,14 @@ for i, element in enumerate(data):
     if "getSize" in element:
         ax.plot(data[element]["count"], data[element]["time"], 'o-', label = element)
 
+title = "Operacja getSize dla wszystkich struktur"
 ax.grid()
 ax.set(xticks = data["ar changeKey"]["count"])
 plt.legend(fontsize = "x-large")
-ax.set_title("Operacja getSize dla wszystkich struktur")
+ax.set_title(title)
 plt.xlabel("count")
 plt.ylabel("time [ns]")
+plt.savefig(title + ".pdf", format="pdf") 
 plt.show()
 
 fig, ax = plt.subplots(figsize=(20, 10))
@@ -89,12 +101,14 @@ for i, element in enumerate(data):
     if "insert" in element:
         ax.plot(data[element]["count"], data[element]["time"], 'o-', label = element)
 
+title = "Operacja insert dla wszystkich struktur"
 ax.grid()
 ax.set(xticks = data["ar changeKey"]["count"])
-ax.set_title("Operacja insert dla wszystkich struktur")
+ax.set_title(title)
 plt.legend(fontsize = "x-large")
 plt.xlabel("count")
 plt.ylabel("time [ns]")
+plt.savefig(title + ".pdf", format="pdf") 
 plt.show()
 
 fig, ax = plt.subplots(figsize=(20, 10))
@@ -102,11 +116,13 @@ for i, element in enumerate(data):
     if "peek" in element:
         ax.plot(data[element]["count"], data[element]["time"], 'o-', label = element)
 
+title = "Operacja peek dla wszystkich struktur"
 ax.grid()
 ax.set(xticks = data["ar changeKey"]["count"])
-ax.set_title("Operacja peek dla wszystkich struktur")
+ax.set_title(title)
 plt.legend(fontsize = "x-large")
 plt.xlabel("count")
 plt.ylabel("time [ns]")
+plt.savefig(title + ".pdf", format="pdf") 
 plt.show()
 
