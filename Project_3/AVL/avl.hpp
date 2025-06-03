96a8f1116aeb71ce336fe7873515316b7fcc72fd
k4jt3k <kajetan.spychala@op.pl>
2025-06-03 16:45:40 +0200

    Update avl.hpp

--- Project_3/AVL/avl.hpp
+++ Project_3/AVL/avl.hpp
@@ -1,22 +1,42 @@
-#ifndef HASHTABLEAVL_H
-#define HASHTABLEAVL_H
+#ifndef AVL_HPP
+#define AVL_HPP
 
-#include <vector>
-#include "HashTable.h"
-#include "avl.hpp"
-
-class HashTableAVL : public HashTable {
-private:
-    std::vector<AVLTree> table;
-    int capacity;
-
-    int hashFunction(int key);
-
+class AVLNode {
 public:
-    HashTableAVL(int size);
-    void insert(int key, int value) override;
-    int search(int key) override;
-    void remove(int key) override;
+    int key;
+    int value;
+    int height;
+    AVLNode* left;
+    AVLNode* right;
+
+    AVLNode(int k, int v) : key(k), value(v), height(1), left(nullptr), right(nullptr) {}
+    
 };
 
-#endif // HASHTABLEAVL_H
+class AVLTree {
+private:
+    AVLNode* root;
+    AVLNode* insert(AVLNode* node, int key, int value);
+    AVLNode* remove(AVLNode* node, int key);
+    AVLNode* rotateRight(AVLNode* y);
+    AVLNode* rotateLeft(AVLNode* x);
+    AVLNode* balance(AVLNode* node);
+    AVLNode* findMin(AVLNode* node);
+    AVLNode* removeMin(AVLNode* node);
+    void destroyTree(AVLNode* node);
+    int height(AVLNode* node);
+    void updateHeight(AVLNode* node);
+    int balanceFactor(AVLNode* node);
+    AVLNode* searchNode(AVLNode* node, int key);
+
+public:
+    AVLTree();
+    ~AVLTree();
+
+    void insert(int key, int value);
+    void remove(int key);
+    int search (int key);
+
+};
+
+#endif
