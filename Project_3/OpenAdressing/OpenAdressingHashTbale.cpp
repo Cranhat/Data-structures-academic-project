1eab18740bdc296a5648fb0bc2a1e686d591388c
k4jt3k <kajetan.spychala@op.pl>
2025-06-03 16:38:56 +0200

    Update OpenAdressingHashTbale.cpp

--- Project_3/OpenAdressing/OpenAdressingHashTbale.cpp
+++ Project_3/OpenAdressing/OpenAdressingHashTbale.cpp
@@ -1,49 +1,72 @@
 #include "OpenAddressingHashTable.hpp"
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
 
-OpenAddressingHashTable::OpenAddressingHashTable(int size) : capacity(size) {
-    table.resize(capacity);
+
+OpenAddressingHashTable::OpenAddressingHashTable(int capacity)
+    : size(0), capacity(capacity) {
+    data = new arrayNode*[capacity];
+    for (int i = 0; i < capacity; i++) {
+        data[i] = nullptr;
+    }
+}
+
+OpenAddressingHashTable::~OpenAddressingHashTable() {
+    clear();
+    delete[] data;
 }
 
 int OpenAddressingHashTable::hashFunction(int key) {
-	return key % capacity;
-}
-
-int OpenAddressingHashTable::probe(int key, int i) {
-	return (hashFunction(key) + i) % capacity;
+    return key % capacity;
 }
 
 void OpenAddressingHashTable::insert(int key, int value) {
-	for (int i = 0; i < capacity; ++i) {
-		int index = probe(key, i);
-		if (table[index].isEmpty || table[index].isDeleted) {
-			table[index] = Entry(key, value);
-			return;
-		}
-	}
+    if (size == capacity) {
+        std::cout << "Hash table full!\n";
+        return;
+    }
+
+    int i = hashFunction(key);
+    while (data[i] != nullptr) {
+        if (data[i]->key == key) {
+            data[i]->value = value; 
+            return;
+        }
+        i = (i + 1) % capacity;
+    }
+
+    data[i] = new arrayNode{key, value};
+    size++;
 }
 
 int OpenAddressingHashTable::search(int key) {
-	for (int i = 0; i < capacity; ++i) {
-		int index = probe(key, i);
-		if (table[index].isEmpty) {
-			return -1; 
-		}
-		if (!table[index].isDeleted && table[index].key == key) {
-			return table[index].value;
-		}
-	}
-	return -1; 
+    int i = hashFunction(key);
+    int count = 0;
+
+    while (data[i] != nullptr && count < capacity) {
+        if (data[i]->key == key)
+            return data[i]->value;
+        i = (i + 1) % capacity;
+        count++;
+    }
+
+    return -1; 
 }
 
 void OpenAddressingHashTable::remove(int key) {
-	for (int i = 0; i < capacity; ++i) {
-		int index = probe(key, i);
-		if (table[index].isEmpty) {
-			return; 
-		}
-		if (!table[index].isDeleted && table[index].key == key) {
-			table[index].isDeleted = true;
-			return;
-		}
-	}
+    int i = hashFunction(key);
+    int count = 0;
+
+    while (data[i] != nullptr && count < capacity) {
+        if (data[i]->key == key) {
+            delete data[i];
+            data[i] = nullptr;
+            size--;
+            return;
+        }
+        i = (i + 1) % capacity;
+        count++;
+    }
+}
 }
